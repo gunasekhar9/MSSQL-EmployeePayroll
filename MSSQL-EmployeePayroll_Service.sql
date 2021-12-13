2524f3c5696100db11532ed979a2db5ce6832c99
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-13 18:17:08 +0530

    [gunasekhar9] add : Ritrieving the data of Salary And Date is Done

--- MSSQL-EmployeePayroll_Service.sql
+++ MSSQL-EmployeePayroll_Service.sql
@@ -8,3 +8,6 @@ INSERT INTO employee_Payrolll (name, salary, start) Values
 ( 'Terisaa', 2000000.00, '2019-11-04' ),
 ( 'Charliee', 300000.00, '2020-05-21' );
 select * from employee_Payrolll
+SELECT name,salary from employee_Payrolll WHERE name = 'BILLL'
+select * from employee_Payrolll
+WHERE start BETWEEN CAST('2021-01-03' AS DATE) AND GETDATE();
