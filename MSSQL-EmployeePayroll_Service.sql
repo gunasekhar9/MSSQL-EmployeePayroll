872b4deaed9c16a8c6886e2018379e50c403b7db
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-13 17:43:05 +0530

    [gunasekhar9] add : Creation Of Employee payroll_Service Table is Done

--- MSSQL-EmployeePayroll_Service.sql
+++ MSSQL-EmployeePayroll_Service.sql
@@ -1,2 +1,4 @@
 create database EmplyeePayroll_Service;
 use EmplyeePayroll_Service;
+create table employee_Payroll( id int IDENTITY(1,1) not null primary key, name varchar(150) not null, salary money not null, start date not null)
+select * from employee_Payroll 
