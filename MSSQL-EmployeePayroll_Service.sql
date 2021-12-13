50130bce09462f20bf2c475a6c95dbaede9ce8d3
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-13 18:01:35 +0530

    [gunasekhar9] add : Insrting Data in EmpPayroll_Service is Done

--- MSSQL-EmployeePayroll_Service.sql
+++ MSSQL-EmployeePayroll_Service.sql
@@ -1,4 +1,9 @@
 create database EmplyeePayroll_Service;
 use EmplyeePayroll_Service;
-create table employee_Payroll( id int IDENTITY(1,1) not null primary key, name varchar(150) not null, salary money not null, start date not null)
-select * from employee_Payroll 
+create table employee_Payrolll( id int IDENTITY(1,1) not null primary key, name varchar(150) not null, salary money not null, start date not null)
+select * from employee_Payrolll
+exec sp_help employee_Payrolll 
+INSERT INTO employee_Payrolll (name, salary, start) Values
+( 'BILLL', 1000000.00,'2021-01-03' ), 
+( 'Terisaa', 2000000.00, '2019-11-04' ),
+( 'Charliee', 300000.00, '2020-05-21' );
