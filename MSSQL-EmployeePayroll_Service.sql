99f9310df3510896ad46b72cc671bd3f4d55451e
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-14 16:32:49 +0530

    [gunasekhar9] add : Extending the Pays of Employees is Done

--- MSSQL-EmployeePayroll_Service.sql
+++ MSSQL-EmployeePayroll_Service.sql
@@ -49,3 +49,17 @@ update employee_Payrolll set Department = 'FINANCE' where name = 'BILLL';
 update employee_Payrolll set Department = 'MARKETING' where name = 'Terisaa';
 update employee_Payrolll set Department = 'PRODUCTION' where name = 'Charliee';
 select * from employee_Payrolll
+
+
+Alter Table employee_Payrolll Add Basic_Pay int;
+Alter Table employee_Payrolll Add Deductions int;
+Alter Table employee_Payrolll Add Taxable_Pay int;
+Alter Table employee_Payrolll Add Income_Tax int;
+Alter Table employee_Payrolll Add Net_Pay int;
+update employee_Payrolll set Basic_Pay = '9000000' where name = 'BILLL';
+update employee_Payrolll set Basic_Pay = '90000' where name = 'Terisaa';
+update employee_Payrolll set Basic_Pay = '29000' where name = 'Charliee';
+update employee_Payrolll set Deductions = '5638' where name = 'Terisaa';
+update employee_Payrolll set Taxable_Pay = '1234' where name = 'Charliee';
+update employee_Payrolll set Income_Tax = '9153' where name = 'BILLL';
+update employee_Payrolll set Net_Pay = '1539' where name = 'BILLL';
