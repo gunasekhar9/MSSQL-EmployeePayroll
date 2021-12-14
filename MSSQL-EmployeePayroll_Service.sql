4469221daa91f726ebc911ec82946d0962fe8f1d
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-14 13:35:25 +0530

    [gunasekhar9] add : Finding the Sum,Avg,Min,Max of Female and Male Employees is Done

--- MSSQL-EmployeePayroll_Service.sql
+++ MSSQL-EmployeePayroll_Service.sql
@@ -1,18 +1,42 @@
 create database EmplyeePayroll_Service;
 use EmplyeePayroll_Service;
+
+
 create table employee_Payrolll( id int IDENTITY(1,1) not null primary key, name varchar(150) not null, salary money not null, start date not null)
 select * from employee_Payrolll
-exec sp_help employee_Payrolll 
+exec sp_help employee_Payrolll
+
+
+ 
 INSERT INTO employee_Payrolll (name, salary, start) Values
 ( 'BILLL', 1000000.00,'2021-01-03' ), 
 ( 'Terisaa', 2000000.00, '2019-11-04' ),
 ( 'Charliee', 300000.00, '2020-05-21' );
+
+
+
 select * from employee_Payrolll
+
+
+
 SELECT name,salary from employee_Payrolll WHERE name = 'BILLL'
 select * from employee_Payrolll
 WHERE start BETWEEN CAST('2021-01-03' AS DATE) AND GETDATE();
+
+
+
 ALTER TABLE employee_Payrolll ADD gender CHAR(1);
 update employee_Payrolll set gender = 'F' where name = 'Terisaa';
 update employee_Payrolll set gender = 'M' where name = 'BILLL' or name = 'Charliee';
 update employee_Payrolll set salary = 900000.00 where name = 'Terisaa';
 select * from employee_Payrolll
+
+
+SELECT AVG(salary) FROM employee_Payrolll WHERE gender = 'M' GROUP BY gender;
+SELECT AVG(salary) FROM employee_Payrolll WHERE gender = 'F' GROUP BY gender;
+SELECT SUM(salary) FROM employee_Payrolll WHERE gender = 'M' GROUP BY gender;
+SELECT SUM(salary) FROM employee_Payrolll WHERE gender = 'F' GROUP BY gender;
+SELECT MIN(salary) FROM employee_Payrolll WHERE gender = 'M' GROUP BY gender;
+SELECT MIN(salary) FROM employee_Payrolll WHERE gender = 'F' GROUP BY gender;
+SELECT MAX(salary) FROM employee_Payrolll WHERE gender = 'M' GROUP BY gender;
+SELECT MAX(salary) FROM employee_Payrolll WHERE gender = 'F' GROUP BY gender;
